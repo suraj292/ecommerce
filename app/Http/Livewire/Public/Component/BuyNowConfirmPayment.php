078151671c7ee89cb8457c10c1674c68503a87c5
suraj292 <surajkumarsharma123@gmail.com>
2022-02-18 18:28:56 +0530

    buy now via cookie done

---
+++ app/Http/Livewire/Public/Component/BuyNowConfirmPayment.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace App\Http\Livewire\Public\Component;
+
+use App\Models\select_product_color;
+use App\Models\user_address;
+use App\Models\user_cart;
+use App\Models\user_order;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Support\Facades\Cookie;
+use Livewire\Component;
+
+class BuyNowConfirmPayment extends Component
+{
+    public $cart, $total, $coupon, $selectedAddress, $user, $productColor, $getColor;
+
+    public function render()
+    {
+        return view('livewire.public.component.buy-now-confirm-payment');
+    }
+    public function mount()
+    {
+        $buyNowItem = json_decode(Cookie::get('buyNow'), true);
+        if (!$buyNowItem){
+            return redirect()->back();
+        }
+        // is for direct buying single product
+        $this->cart = $buyNowItem;
+        $colorId = $buyNowItem['product_color_id'];
+        $this->getColor = select_product_color::select('color_image')->find($colorId);
+
+        $this->user = Auth::user();
+        $coupon = Cookie::get('coupon');
+        if ($coupon){
+            $this->coupon = json_decode($coupon, true);
+            $this->total = Cookie::get('total');
+        }
+        $addId = json_decode(Cookie::get('selectedAddress'));
+
+        if (is_null($addId)){
+            $addressId = user_address::select('id')->where('user_id', Auth::id())->where('default', true)->first();
+            if (is_null($addressId)){
+                return redirect('checkout')->with('empty_address', 'Please select address.');
+            }else {
+                $addressId = $addressId->id;
+            }
+        }else{
+            $addressId = $addId;
+        }
+
+        if (request('razorpay_payment_id')){
+            /*
+            * generate order and invoice number
+            * format : ORD/INV year month (last_order + 1)
+            * eg: ORD2021110001
+            * NOTE: first order will created by default to avoid database sql error
+            */
+            $newCart = user_cart::create($buyNowItem);
+            $lastOrder = user_order::select('id')->latest('id')->first();
+            $num = $lastOrder->id + 1;
+            $order = date("Y").date("m").sprintf("%04d", $num);
+
+            //$data = coupon code, coupon discount, total Amount & GST
+            $data = json_decode(Cookie::get('confirmPayment'), true);
+
+            $userOrder = new user_order([
+                'razorpay_id' => request('razorpay_payment_id'),
+                'user_id' => Auth::id(),
+                'user_delivery_id' => $addressId,
+                'i_think_logistics_id' => null,
+                //this will get all ordered product [eg="1,2"]
+                'product_user_cart_ids' => $newCart->id,
+                'coupon_code' => $data['couponCode'],
+                'coupon_discount' => $data['couponValue'],
+                'order_number' => 'ORD'.$order,
+//                'invoice_number' => 'INV'.$order,
+                'total_payable_cost' => $data['total'],
+                'gst_charge' => $data['gst'],
+            ]);
+            $userOrder->save();
+            Cookie::queue('orderSuccess', $userOrder->id, 100*2);
+//            $cart = user_cart::where('user_id', Auth::id());
+//            $cart->delete();
+            $newCart->delete();
+            $this->redirect(route('order-success'));
+
+        }
+
+    }
+
+    public function checkoutCod()
+    {
+        $addId = json_decode(Cookie::get('selectedAddress'));
+        if (is_null($addId)){
+            $addressId = user_address::select('id')->where('user_id', Auth::id())->where('default', true)->first();
+            $addressId = $addressId->id;
+        }else{
+            $addressId = $addId;
+        }
+        $lastOrder = user_order::select('id')->latest('id')->first();
+        $num = $lastOrder->id + 1;
+        $order = date("Y").date("m").sprintf("%04d", $num);
+        //$data = coupon code, coupon discount, total Amount & GST
+        $data = json_decode(Cookie::get('confirmPayment'), true);
+        $userOrder = new user_order([
+            'user_id' => Auth::id(),
+            'user_delivery_id' => $addressId,
+            'i_think_logistics_id' => null,
+            //this will get all ordered product [eg="1,2"]
+            'product_user_cart_ids' => $this->cart->implode('id', ','),
+            'coupon_code' => $data['couponCode'],
+            'coupon_discount' => $data['couponValue'],
+            'order_number' => 'ORD'.$order,
+//            'invoice_number' => 'INV'.$order,
+            'total_payable_cost' => $data['total'],
+            'gst_charge' => $data['gst'],
+        ]);
+        $userOrder->save();
+        Cookie::queue('orderSuccess', $userOrder->id, 100 * 2);
+        $cart = user_cart::where('user_id', Auth::id());
+        $cart->delete();
+        $this->redirect(route('order-success'));
+    }
+}
---
+++ resources/views/livewire/public/component/buy-now-confirm-payment.blade.php
@@ -0,0 +1,217 @@
+<div>
+    <div class="hidden-hight-div"></div>
+    <!-- breadcrumb start -->
+    <div class="breadcrumb-section">
+        <div class="container">
+            <div class="row">
+                <div class="col-sm-6">
+                    <div class="page-title">
+                        <h2>cart</h2>
+                    </div>
+                </div>
+                <div class="col-sm-6">
+                    <nav aria-label="breadcrumb" class="theme-breadcrumb">
+                        <ol class="breadcrumb">
+                            <li class="breadcrumb-item"><a href="{{ route('home') }}">Home</a></li>
+                            <li class="breadcrumb-item active">Confirm Payment</li>
+                        </ol>
+                    </nav>
+                </div>
+            </div>
+        </div>
+    </div>
+    <!-- breadcrumb End -->
+
+
+    <!--section start-->
+    <section class="cart-section section-b-space">
+        <div class="container">
+            @if(!is_null($cart))
+                <div class="row">
+                    <div class="col-sm-12">
+                        <div class="cart_counter">
+                            <div class="countdownholder">
+                                Your cart will be expired in<span id="timer"></span> minutes!
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-12 grid-margin">
+                        <div class="card">
+                            <div class="card-body">
+                                <table class="table cart-table">
+                                    <thead>
+                                    <tr>
+                                        <th> Product </th>
+                                        <th> Color </th>
+                                        <th> Amount </th>
+                                        <th> Total </th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                    <span style="display: none;">
+                                        {{ $subtotal = 0, $savings = 0 }}
+                                        {{ $productWithoutOffer = 0, $maximumAmount=0 }}
+                                    </span>
+{{--                                    @foreach($cart as $product)--}}
+                                        <tr>
+                                            <td>
+                                                <a href="{{ route('product_details', Str::slug($cart['title'])) }}">
+                                                    <img src="{{asset('storage/product/small/'.$cart['image'])}}" alt="Cart Product" style="height: 50px;">
+                                                    {{ $cart['title'] }} <span style="color: #0a0a0a; text-transform: none;">x {{ $cart['quantity'] }}</span>
+                                                </a>
+                                            </td>
+                                            <td>
+                                                <img src="{{ asset('storage/color_image/'.$getColor['color_image']) }}" width="30px">
+                                            </td>
+                                            <td style="color: orangered; font-size: 20px;">
+                                                @if($cart['offer_price'])
+                                                    {{--    this span for calculating total saving of offer product     --}}
+                                                    <span style="display: none;">
+                                                        {{ $offerSavings = $cart['price'] - $cart['offer_price'], $couponOffer = 0 }}
+                                                        {{ $totalAmount = $cart['price'] * $cart['quantity'] }}
+                                                    </span>
+
+                                                    &#8377; {{ $cost = $cart['offer_price'] }} <del style="font-size: 14px;">&#8377; {{ $cart['price'] }}</del>
+                                                @else
+                                                    <span style="display: none;">
+                                                        {{ $offerSavings = 0, $couponOffer = $totalAmount = $cart['price'] * $cart['quantity'] }}
+                                                    </span>
+
+                                                    &#8377; {{ $cost = $cart['price'] }}
+                                                @endif
+                                            </td>
+                                            <td style="color: orangered;">
+                                                &#8377; {{ $price = $cart['quantity'] * $cost }}
+                                            </td>
+                                        </tr>
+                                        <span style="display: none;">
+                                            {{ $subtotal += $price, $savings += $offerSavings }}
+                                            {{ $productWithoutOffer += $couponOffer, $maximumAmount += $totalAmount }}
+                                        </span>
+                                        {{--                                        {{ $collections []= collect([ 'productId'=>$product->product_id, 'productQty'=>$product->quantity, 'productColor'=>$product->product_color_id ]) }}--}}
+{{--                                    @endforeach--}}
+                                    </tbody>
+                                </table>
+                                {{--                                {{ dd($collections) }}--}}
+                                <div class="row justify-content-end">
+                                    <div class="col-sm-12 col-md-6 col-lg-4">
+                                        <div class="checkout-details pt-4">
+                                            <div class="order-box">
+                                                <ul class="qty" style="border: none;">
+                                                    <li style="display: none;">
+                                                        {{ $netAmount = $subtotal * 100/118, $gst = $subtotal - $netAmount }}
+                                                    </li>
+                                                    <li>Net Amount <span>&#8377; {{ round($netAmount) }}</span></li>
+                                                    <li>GST (18%) <span>&#8377; {{ round($gst) }}</span></li>
+                                                    @if($coupon && $productWithoutOffer * $coupon['value'] > 0)
+                                                        {{--    this li for cappin coupon max discount      --}}
+                                                        <li style="display: none;">
+                                                            {{ $maxCouponDiscount = $coupon['max_amount'], $couponDiscount = $productWithoutOffer * $coupon['value'] / 100 }}
+                                                            {{ $maxCouponDiscount < $couponDiscount ? $discount=$maxCouponDiscount : $discount=$couponDiscount }}
+                                                        </li>
+
+                                                        <li style="color: #00a20c;">Coupon ({{ strtoupper($coupon['code']) }})
+                                                            <span style="color: #00a20c;">&#8377; {{ $discount }} ({{ $coupon['value'] }}%)</span>
+                                                        </li>
+                                                    @endif
+                                                    @if($productWithoutOffer > 0 && $coupon)
+                                                        <li style="color: #00a20c;">Total Saving <span style="color: #00a20c;">&#8377; {{ $savings + $discount }}</span></li>
+                                                    @elseif($savings > 0)
+                                                        <li style="color: #00a20c;">Total Saving <span style="color: #00a20c;">&#8377; {{ $savings }}</span></li>
+                                                    @endif
+                                                    <li style="color: orangered;">Total
+                                                        <span style="color: orangered;">
+                                                            &#8377; {{ $finalCost = $total != null ? $total : $subtotal }}
+                                                            <del>&#8377; {{ $maximumAmount }}</del>
+                                                        </span>
+                                                    </li>
+                                                </ul>
+                                            </div>
+
+                                        </div>
+                                    </div>
+                                    <div class="col-sm-12 col-md-6 col-lg-4">
+                                        <div class="payment-box">
+                                            <div class="upper-box">
+                                                <div class="payment-options">
+                                                    <ul>
+                                                        <li>
+                                                            <div class="radio-option">
+                                                                <input type="radio" name="payment-group" id="payment-2">
+                                                                <label for="payment-2">Cash On Delivery<span class="small-text">Please send a check to Store
+                                                                    Name, Store Street, Store Town, Store State /
+                                                                    County, Store Postcode.</span></label>
+                                                            </div>
+                                                        </li>
+                                                        <li>
+                                                            <div class="radio-option paypal">
+                                                                <input type="radio" name="payment-group" id="payment-3" checked>
+                                                                <label for="payment-3"> Debit / Credit Card / Net-Banking / UPI / Wallet
+                                                                    <span class="image"><img src="{{ asset('assets/images/paypal.png') }}" alt=""></span>
+                                                                </label>
+                                                            </div>
+                                                        </li>
+                                                    </ul>
+                                                </div>
+                                            </div>
+                                            <div class="text-end">
+                                                {{--                                                <a href="#" class="btn-solid btn">Place Order</a>--}}
+                                                <form {{--wire:submit.prevent="getRazorpayResponse"--}} id="prepaid">
+                                                    <script src="https://checkout.razorpay.com/v1/checkout.js"
+                                                            data-key="{{ env('RAZORPAY_KEY') }}"
+                                                            data-amount="{{ $finalCost * 100 }}"
+                                                            data-buttontext="Confirm & Pay"
+                                                            data-name="HouseOfBhavana"
+                                                            data-description="Rozerpay"
+                                                            {{--                                                            data-image="https://www.itsolutionstuff.com/frontTheme/images/logo.png"--}}
+                                                            data-prefill.name="{{ $user->name }}"
+                                                            data-prefill.contact="{{ $user->mobile }}"
+                                                            data-prefill.email="{{ $user->email }}"
+                                                        {{--                                                            data-theme.color="#ff7529"--}}
+                                                    >
+                                                    </script>
+                                                </form>
+                                                <div id="cod" style="display: none;">
+                                                    <a href="#" class="btn-solid btn" wire:click="checkoutCod">Confirm</a>
+                                                </div>
+                                            </div>
+                                            <script>
+                                                $(document).ready(function (){
+                                                    $(".razorpay-payment-button").addClass(' btn-solid btn');
+                                                    $("#payment-2").on('click', function (){
+                                                        $("#prepaid").hide();
+                                                        $("#cod").show();
+                                                    });
+                                                    $("#payment-3").on('click', function (){
+                                                        $("#cod").hide();
+                                                        $("#prepaid").show();
+                                                    });
+                                                });
+                                            </script>
+                                        </div>
+                                    </div>
+                                    <span style="display: none;">
+                                        {{ $couponCode = $coupon['code'] ?? null , $couponValue = $discount ?? null }}
+                                        {{ $data = collect(['total'=>$finalCost,'gst'=>round($gst),'couponCode'=>$couponCode, 'couponValue'=>$couponValue]) }}
+                                        {{ \Illuminate\Support\Facades\Cookie::queue('confirmPayment', $data, 5) }}
+                                    </span>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+
+                </div>
+            @endif
+        </div>
+    </section>
+    <!--section end-->
+</div>
+@section('style')
+    <style>
+        .ti-trash{font-size: 22px;}
+        .ti-trash:hover{color: red;}
+    </style>
+@endsection
+@section('script')
+    <script src="{{ asset('assets/js/timer1.js') }}"></script>
+@endsection
