7a9c0abb61383e0def7781a82c875458256b0156
suraj292 <surajkumarsharma123@gmail.com>
2022-06-06 20:44:48 +0530

    wrong otp error msg

--- app/Http/Livewire/Admin/Stocks.php
+++ app/Http/Livewire/Admin/Stocks.php
@@ -6,8 +6,15 @@ use Livewire\Component;
 
 class Stocks extends Component
 {
+    public $stocks;
     public function render()
     {
-        return view('livewire.admin.stocks');
+        return view('livewire.admin.stocks')->layout('layouts.admin');
     }
+
+    public function mount()
+    {
+
+    }
+
 }
