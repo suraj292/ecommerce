483606ffec61b4f12489b19d0302b23827fc0a36
suraj292 <surajkumarsharma123@gmail.com>
2022-08-25 10:00:34 +0530

    Update email_verification.blade.php
    corrected link changed by sendgrid

--- resources/views/mail/email_verification.blade.php
+++ resources/views/mail/email_verification.blade.php
@@ -5,12 +5,12 @@
 <h4>The Link will expire after 15 Minutes of receiving.</h4>
 
 <p>
-    <a href="{{ route('home').'/'.'verification'.'/'.$userId.'/'.$link }}"> Click Here </a>
+    <a clicktracking="off" href="{{ route('home').'/'.'verification'.'/'.$userId.'/'.$link }}"> Click Here </a>
     to Verify your account or Copy Past below link in browser.
 </p>
 
 <hr>
 
 <p>
-    final link: <a href="{{ route('home').'/'.'verification'.'/'.$userId.'/'.$link }}">{{ route('home').'/'.'verification'.'/'.$userId.'/'.$link }}</a>
+    final link: <a clicktracking="off" href="{{ route('home').'/'.'verification'.'/'.$userId.'/'.$link }}">{{ route('home').'/'.'verification'.'/'.$userId.'/'.$link }}</a>
 </p>
