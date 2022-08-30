46b954225acc1862176dfd186cb8e94a5b13ea9a
suraj sharma <surajkumarsharma123@gmail.com>
2022-08-30 17:41:41 +0530

    commit from office 30 Aug | logistics rate form created

--- app/Http/Livewire/Admin/LogisticsRate.php
+++ app/Http/Livewire/Admin/LogisticsRate.php
@@ -6,9 +6,26 @@ use Livewire\Component;
 
 class LogisticsRate extends Component
 {
+    public $fromPin='201005', $toPin, $length, $width, $height, $weight, $paymentMode, $mrp;
+
     public function render()
     {
         return view('livewire.admin.logistics-rate')->layout('layouts.admin');
+    }
+
+    public function checkLogisticsRate()
+    {
+        $this->validate([
+            'fromPin'=>'required|digits:6',
+            'toPin'=>'required|digits:6',
+            'length'=>'required',
+            'width'=>'required',
+            'height'=>'required',
+            'weight'=>'required',
+            'paymentMode'=>'required',
+            'mrp'=>'required'
+        ]);
+
     }
 
 }
--- resources/views/livewire/admin/logistics-rate.blade.php
+++ resources/views/livewire/admin/logistics-rate.blade.php
@@ -13,8 +13,99 @@
             {{--    CATEGORY    --}}
             <div class="col-md-12 col-sm-12 grid-margin stretch-card">
                 <div class="card">
-                    <div class="card-body" style="/*max-height: 300px;*/ overflow-y: auto;" wire:ignore>
-                        <h4 class="card-title">Orders</h4>
+                    <div class="card-body" style="overflow-y: auto;">
+                        <h4 class="card-title">Logistics Rate</h4>
+                        <form wire:submit.prevent="checkLogisticsRate">
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">From Pincode</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="201005" wire:model.defer="fromPin">
+                                            @error('fromPin')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">To Pincode</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="201005" wire:model.defer="toPin">
+                                            @error('toPin')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Shipment Length</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="length in cm" wire:model.defer="length">
+                                            @error('length')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Shipment width</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="width in cm" wire:model.defer="width">
+                                            @error('width')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Shipment height</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="height in cm" wire:model.defer="height">
+                                            @error('height')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Shipment weight</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="weight in kg" wire:model.defer="weight">
+                                            @error('weight')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="row">
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">Payment Mode</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="prepaid/cod" wire:model.defer="paymentMode">
+                                            @error('paymentMode')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                                <div class="col-md-6">
+                                    <div class="form-group row">
+                                        <label class="col-sm-3 col-form-label">MRP &#8377;</label>
+                                        <div class="col-sm-9">
+                                            <input type="text" class="form-control" placeholder="&#8377; xxxx" wire:model.defer="mrp">
+                                            @error('mrp')<p class="text-danger mt-1">{{ $message }}</p>@enderror
+                                        </div>
+                                    </div>
+                                </div>
+                            </div>
+                            <button type="submit" class="btn btn-gradient-primary btn-rounded btn-fw">Check</button>
+                        </form>
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-12 col-sm-12 grid-margin stretch-card">
+                <div class="card">
+                    <div class="card-body" style="overflow-y: auto;">
+                        <h4 class="card-title">Logistics Rate</h4>
+
                     </div>
                 </div>
             </div>
